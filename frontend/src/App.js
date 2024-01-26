a5f621d496aa1bcaf2dc4842e009fb39a59e6a49
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-01-26 10:17:42 +0100

    begin projet structure and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Route,Routes}from "react-router-dom"
+import { Customer } from './Pages/Customer/Customer';
+import { Home } from './Pages/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fiverr project
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/Customer" element={<Customer />} />
+</Routes>
+
+      </BrowserRouter>
+      
+     
     </div>
   );
 }
---
+++ frontend/src/Pages/Customer/Customer.js
@@ -0,0 +1,7 @@
+export const Customer=()=>{
+    return (
+    <div>
+        <h1>Customer</h1>
+    </div>
+    )
+}
