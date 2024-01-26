e84b8b83b89c4c0bf718118c10fe84545525e977
imranemb <imranentv92@outlook.com>
2024-01-26 11:24:47 +0100

    Navbar sample

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 
 import './App.css';
-import {BrowserRouter, Route,Routes}from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Customer } from './Pages/Customer/Customer';
 import { Home } from './Pages/Home/Home';
+import NavbarClient from './Pages/NavbarClient/NavbarClient';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/Customer" element={<Customer />} />
-</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Customer" element={<Customer />} />
+          <Route path="/Navbar" element={<NavbarClient />} />
+        </Routes>
 
       </BrowserRouter>
-      
-     
+
+
     </div>
   );
 }
