e69c7a07a148dcbe6bb60991e276a7a8505dfaec
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-01-26 14:38:00 +0100

    connection

--- backend/src/Controller/TestController.php
+++ backend/src/Controller/TestController.php
@@ -11,8 +11,9 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): JsonResponse
     {
+        //send json data
         return $this->json([
-            'test' => 'works fine',
+            'test' => 'aziz fine',
             'test2' => '7out azraq',
             'test3' => 'works fine',
         ]);
