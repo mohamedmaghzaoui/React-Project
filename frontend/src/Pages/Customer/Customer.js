0efab83d6750af4967ef29bbf3514d8aa003b490
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-01-30 09:38:13 +0100

    test branches

--- frontend/src/Pages/Customer/Customer.js
+++ frontend/src/Pages/Customer/Customer.js
@@ -7,19 +7,20 @@ export const Customer = () => {
   const url = "http://localhost:8000/test";
   //useState initialize to null
   const [TestData, SetTestData] = useState(null);
+  //get data from symfony server
+  async function fetchData() {
+    try {
+      const apiResponse = await axios.get(url);
+      SetTestData(apiResponse.data.test);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
 //useffect hook 
   useEffect(() => {
     // Use useEffect to make the API request when the component mounts
-    async function fetchData() {
-      try {
-        const apiResponse = await axios.get(url);
-        SetTestData(apiResponse.data);
-        console.log(apiResponse.data); // Log the response data
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    }
-
+    
     fetchData(); // Call the fetchData function
   }, []); // The empty array [] ensures the effect runs once on component mount
 
@@ -28,13 +29,17 @@ export const Customer = () => {
     <div>
       <h1>Customer</h1>
      
-      {console.log(TestData)}
+    
       {TestData && (
+        TestData.map((value)=>
         <div>
-          <p>Test: {TestData.test}</p>
-          <p>Test2: {TestData.test2}</p>
-          <p>Test3: {TestData.test3}</p>
+            <p>{value.name}</p>
+            <p>{value.age}</p>
         </div>
+      
+        
+        
+        )
       )}
     </div> );
 };
