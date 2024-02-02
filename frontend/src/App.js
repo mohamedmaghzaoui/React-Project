45a7e661b583e74c5eeee2b9422e0c2e1601da6b
azizkab <azizkabissa007@gmail.com>
2024-02-02 13:13:47 +0100

    freelancer form page icon route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Customer } from "./Pages/Customer/Customer";
 import { Home } from "./Pages/Home/Home";
 import MainNavbar from "./Pages/MainNavbar/MainNavbar";
+import {Freelancer} from "./Pages/Freelancer/Freelancer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -16,6 +17,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/Customer" element={<Customer />} />
+              <Route path="/Freelancer" element={<Freelancer />} />
             </Routes>
           </div>
         </div>
--- frontend/src/Pages/MainNavbar/MainNavbar.js
+++ frontend/src/Pages/MainNavbar/MainNavbar.js
@@ -1,79 +1,68 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaHome, FaBriefcase, FaBell, FaPlus, FaBars, FaTimes } from "react-icons/fa";
 import "./MainNavbar.css";
-import { FaHome, FaBell, FaBriefcase, FaBars, FaTimes } from "react-icons/fa";
-import { useState } from "react";
 
 function MainNavbar() {
-  //show or hide icon state
   const [navIcon, setNavIcon] = useState("bars");
-  //show or hide icon navbar line
-  const [linesOpacities, setLinesOpacities] = useState([1, 0, 0]);
-  //change colors of navbar icon
-  const [iconColors, setIconeColors] = useState(["#D62929", "white", "white"]);
+  const [linesOpacities, setLinesOpacities] = useState([1, 0, 0, 0]);
+  const [iconColors, setIconeColors] = useState(["#D62929", "white", "white", "white"]);
 
   return (
-    //begin navbar
-    <div
-      id="navbar"
-      className="col-xl-1 col-lg-1 col-12 py-4"
-      style={{ height: "100vh" }}
-    >
+    <div id="navbar" className="col-xl-1 col-lg-1 col-12 py-4" style={{ height: "100vh" }}>
       {/* first icon*/}
       <div
         className="px-2 "
-        //function for dynamic styling show or hide line and change colors
         onClick={() => {
-          setLinesOpacities([1, 0, 0]);
-          setIconeColors(["#D62929", "white", "white"]);
+          setLinesOpacities([1, 0, 0, 0]);
+          setIconeColors(["#D62929", "white", "white", "white"]);
         }}
       >
         <Link className="link d-block text-center" to={"/"}>
           <span className={`line py-1 opacity-${linesOpacities[0]} `}></span>
-
-          <FaHome
-            className="my-4 "
-            style={{ color: `${iconColors[0]}` }}
-            size={35}
-          />
+          <FaHome className="my-4" style={{ color: `${iconColors[0]}` }} size={35} />
         </Link>
       </div>
       {/* second icon*/}
       <div
         className="px-2"
-        //function for dynamic styling show or hide line and change colors
         onClick={() => {
-          setLinesOpacities([0, 1, 0]);
-          setIconeColors(["White", "#D62929", "white"]);
+          setLinesOpacities([0, 1, 0, 0]);
+          setIconeColors(["White", "#D62929", "white", "white"]);
         }}
       >
         <Link className="link d-block text-center" to={"/Customer"}>
           <span className={`line py-1 opacity-${linesOpacities[1]} `}></span>
-          <FaBriefcase
-            className="my-4 "
-            style={{ color: `${iconColors[1]}` }}
-            size={35}
-          />
+          <FaBriefcase className="my-4" style={{ color: `${iconColors[1]}` }} size={35} />
         </Link>
       </div>
       {/* third icon*/}
       <div
         className="px-2"
-        //function for dynamic styling show or hide line and change colors
         onClick={() => {
-          setLinesOpacities([0, 0, 1]);
-          setIconeColors(["white", "white", "#D62929"]);
+          setLinesOpacities([0, 0, 1, 0]);
+          setIconeColors(["white", "white", "#D62929", "white"]);
         }}
       >
         <Link className="link d-block text-center" to={"/"}>
           <span className={`line py-1 opacity-${linesOpacities[2]} `}></span>
-          <FaBell
-            className="my-4 "
-            style={{ color: `${iconColors[2]}` }}
-            size={35}
-          />
+          <FaBell className="my-4" style={{ color: `${iconColors[2]}` }} size={35} />
         </Link>
       </div>
-    </div> //end navbar
+      {/* fourth icon (project form) */}
+      <div
+        className="px-2"
+        onClick={() => {
+          setLinesOpacities([0, 0, 0, 1]);
+          setIconeColors(["white", "white", "white", "#D62929"]);
+        }}
+      >
+        <Link className="link d-block text-center" to={"/Freelancer"}>
+          <span className={`line py-1 opacity-${linesOpacities[3]} `}></span>
+          <FaPlus className="my-4" style={{ color: `${iconColors[3]}` }} size={35} />
+        </Link>
+      </div>
+    </div>
   );
 }
 
