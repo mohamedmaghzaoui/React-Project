58a93f9259c964b064efc57b80af15c42a6d836c
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-02-07 19:49:51 +0100

    nearly finished home page

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -11,14 +11,27 @@ export const Home = () => {
     //begin home page
     <div id={styles.home} className="  container-fluid    ">
       <br />
+      <div style={{ marginTop: "7%" }} className="row container-fluid">
+        <div className="py-5 px-5 col-8 ">
+          <h1 className=" text-light     fw-bold">Find the right Freelance</h1>
+          <h1 className=" text-light  fw-bold">do the right Work</h1>
+          <p className="text-light fw-semibold fs-5">
+            Work with the best freelance talent from around the world on our
+            secure,
+            <p> flexible and cost-effective platform.</p>
+          </p>
 
-      <div style={{ marginTop: "7%" }} className="py-5 px-5  ">
-        <h1 className=" text-light     fw-bold">Find the right Freelance</h1>
-        <h1 className=" text-light  fw-bold">do the right Work</h1>
-
-        <div class="row input-group-lg">
-          <input type="text" className=" form-control w-50" />
-          <button class="col-1 btn btn-success">search</button>
+          <div class="row input-group-lg py-2">
+            <input type="text" className=" form-control w-75" />
+            <button class="col-2 btn btn-success">search</button>
+          </div>
+        </div>
+        <div className="col">
+          <img
+            className="img-fluid "
+            src="https://res.cloudinary.com/upwork-cloud-acquisition-prod/image/upload/c_scale,w_440,h_300,f_auto,q_auto,dpr_2.0/brontes/hero/searching-talent@1x.png"
+            alt=""
+          />
         </div>
       </div>
     </div>
