d103a360b1beb0d88caf71ed36952df0542ccb8c
imranemb <imranentv92@outlook.com>
2024-02-08 11:08:50 +0100

    added footer  to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Home } from "./Pages/Home/Home";
 import MainNavbar from "./Pages/MainNavbar/MainNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import { ServicesIconsList } from "./Pages/Home/Components/ServicesIconsList";
 
 function App() {
   return (
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -4,11 +4,13 @@ import React from "react";
 import { Cards } from "./Components/Card";
 //styles using css modules
 import styles from "./Css/home.module.css";
-
+import { ServicesIconsList } from "./Components/ServicesIconsList";
+import { Footer } from "./Components/Footer";
 export const Home = () => {
   return (
     //begin home page container
-    <div id={styles.home} className="  container-fluid    ">
+    <div>
+      <div id={styles.home} className="  container-fluid    ">
       <br />
       {/* begin row to divide main element*/}
       <div style={{ marginTop: "7%" }} className="row container-fluid">
@@ -41,6 +43,28 @@ export const Home = () => {
         {/* end elements*/}
       </div>{" "}
       {/* n end row to divide main element*/}
+    </div>
+    <div className="  container-fluid    ">
+      <h3 style={{ margin:"5%", marginBottom: "2%"}}>You need it, we've got it</h3>
+      <div className="d-flex flex-wrap justify-content-center">
+        <ServicesIconsList name="Graphics" src="https://cdn-icons-png.flaticon.com/512/4091/4091459.png" href=""/>
+        <ServicesIconsList name="Marketing" src="https://cdn-icons-png.flaticon.com/512/3408/3408823.png" href=""/>
+        <ServicesIconsList name="Translation" src="https://cdn-icons-png.flaticon.com/512/2793/2793765.png" href=""/>
+        <ServicesIconsList name="Video" src="https://w7.pngwing.com/pngs/314/584/png-transparent-computer-icons-video-display-resolution-others-angle-text-rectangle-thumbnail.png" href=""/>
+        </div>
+        <div className="d-flex flex-wrap justify-content-center">
+        <ServicesIconsList name="Programming" src="https://p7.hiclipart.com/preview/734/59/634/computer-icons-programmer-computer-programming-source-code-coder.jpg" href=""/>
+        <ServicesIconsList name="Business" src="https://cdn-icons-png.flaticon.com/512/2747/2747468.png" href=""/>
+        <ServicesIconsList name="Lifestyle" src="https://static.thenounproject.com/png/5708815-200.png" href=""/>
+        <ServicesIconsList name="Photography" src="https://www.freeiconspng.com/uploads/photography-icon-png-0.png" href=""/>
+
+      </div>
+    </div>
+    <div id={styles.footer} className="  container-fluid    ">
+
+  <Footer categoryname="Category" name1="Graphics" name2="Marketing" name3="Translation" name4="Video" name5="Programming" name6="Business" name7="Lifestyle" name8="Photography"/>
+
+    </div>
     </div>
     //end home page container
   );
