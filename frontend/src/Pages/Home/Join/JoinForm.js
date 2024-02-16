46fa7779010e71d7722ada3f0eaa45d4070ab313
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2024-02-16 14:22:56 +0100

    added overlay style

--- frontend/src/Pages/Home/Join/JoinForm.js
+++ frontend/src/Pages/Home/Join/JoinForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { RegisterForm } from "./components/registerForm"; //first form
 import { UsernameForm } from "./components/usernameForm"; //second form
+import styles from "../Css/overlay.module.css";
 
 //style
 const JOIN_STYLE = {
@@ -28,42 +29,44 @@ export default function Join({ openJoin, onCloseJoin }) {
   const [form, setForm] = useState("register"); //form state used to either show first register form or second username form
   if (!openJoin) return null;
   return (
-    <div style={JOIN_STYLE}>
-      <button
-        style={{
-          position: "absolute",
-          top: "10px",
-          right: "10px",
-          backgroundColor: "transparent",
-          border: "none",
-          cursor: "pointer",
-          fontSize: "1.5rem",
-        }}
-        //hide form
-        onClick={onCloseJoin}
-      >
-        &times;
-      </button>
-      <div style={IMAGE_STYLE}>
-        <img
-          src="https://imgs.search.brave.com/x6iqbJWLvymDlIcHHDsXqFoo6fP5UH86u2gqwTLL17U/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA0LzYyLzUzLzM0/LzM2MF9GXzQ2MjUz/MzQxMF9lNXZNRk9t/eEJHRmtCN3dEV1FE/RVd5WVhETnZ2Tndx/bi5qcGc"
-          alt="Join Image"
-          style={{ width: "100%" }}
-        />
+    <div className={styles.overlay}>
+      <div style={JOIN_STYLE}>
+        <button
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "1.5rem",
+          }}
+          //hide form
+          onClick={onCloseJoin}
+        >
+          &times;
+        </button>
+        <div style={IMAGE_STYLE}>
+          <img
+            src="https://imgs.search.brave.com/x6iqbJWLvymDlIcHHDsXqFoo6fP5UH86u2gqwTLL17U/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA0LzYyLzUzLzM0/LzM2MF9GXzQ2MjUz/MzQxMF9lNXZNRk9t/eEJHRmtCN3dEV1FE/RVd5WVhETnZ2Tndx/bi5qcGc"
+            alt="Join Image"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <h2>Join us</h2>
+        {/* condition to either show first or second form */}
+        {form === "register" ? (
+          //send props set userdata  and form state
+          <RegisterForm setUserData={setUserData} />
+        ) : (
+          //send props of  set Form userdata state and closeform function
+          <UsernameForm
+            closeForm={onCloseJoin}
+            setForm={setForm}
+            userData={userData}
+          />
+        )}
       </div>
-      <h2>Join us</h2>
-      {/* condition to either show first or second form */}
-      {form === "register" ? (
-        //send props set userdata  and form state
-        <RegisterForm setForm={setForm} setUserData={setUserData} />
-      ) : (
-        //send props of  set Form userdata state and closeform function
-        <UsernameForm
-          closeForm={onCloseJoin}
-          setForm={setForm}
-          userData={userData}
-        />
-      )}
     </div>
   );
 }
--- frontend/src/Pages/Home/Login/loginForm.js
+++ frontend/src/Pages/Home/Login/loginForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import style from "../Css/overlay.module.css";
 
 const LOGIN_STYLE = {
   position: "fixed",
@@ -84,51 +85,77 @@ export default function Login({ openLogin, onCloseLogin }) {
     <div className="form" onClick={handleOutsideClick} style={FORM_STYLE}>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
-          <input type="text" name="uname" className="form-control" id="exampleInputEmail1" required />
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            name="uname"
+            className="form-control"
+            id="exampleInputEmail1"
+            required
+          />
           {renderErrorMessage("uname")}
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" name="pass" className="form-control" id="exampleInputPassword1" required />
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="pass"
+            className="form-control"
+            id="exampleInputPassword1"
+            required
+          />
           {renderErrorMessage("pass")}
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
 
   if (!openLogin) return null;
   return (
-    <div style={LOGIN_STYLE}>
-      <button
-        style={{
-          position: "absolute",
-          top: "10px",
-          right: "10px",
-          backgroundColor: "transparent",
-          border: "none",
-          cursor: "pointer",
-          fontSize: "1.5rem",
-        }}
-        onClick={onCloseLogin}
-      >
-        &times;
-      </button>
-      <div style={IMAGE_STYLE}>
-        <img
-          src="https://www.shutterstock.com/image-vector/man-key-near-computer-account-260nw-1499141258.jpg"
-          alt="Login Image"
-          style={{ width: "100%" }}
-        />
-      </div>
-      <div style={FORM_STYLE}>
-        <div style={{ fontSize: "1.5rem", marginBottom: "20px" }}>Login</div>
-        {renderForm}
+    <div className={style.overlay}>
+      <div style={LOGIN_STYLE}>
+        <button
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "1.5rem",
+          }}
+          onClick={onCloseLogin}
+        >
+          &times;
+        </button>
+        <div style={IMAGE_STYLE}>
+          <img
+            src="https://www.shutterstock.com/image-vector/man-key-near-computer-account-260nw-1499141258.jpg"
+            alt="Login Image"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div style={FORM_STYLE}>
+          <div style={{ fontSize: "1.5rem", marginBottom: "20px" }}>Login</div>
+          {renderForm}
+        </div>
       </div>
     </div>
   );
