4d60297b4f79a07b3cbae595f6a40c3f6b7fd8c0
adem-sassi <sassiadem7@gmail.com>
2024-02-29 14:54:52 +0100

    fixing gigs page

--- frontend/src/Pages/gigCard/Components/GigCard.js
+++ frontend/src/Pages/gigCard/Components/GigCard.js
@@ -1,44 +1,38 @@
 import React from "react";
-import './GigCard.css';
-import like from "./like.png";
+import "./GigCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import star from "./star.png";
-
 import { Link } from "react-router-dom";
 
-const GigCard = ({item, index}) => {
-    console.log(index);
-    return (
-        <Link to="/gig" className="link">
-            <div className='gigCard'>
-                <img src={item.pp} alt="Projet" />
-                <div className="info">
-                    <div className="user">
-                        <img src={item.pp} alt="" />
-                        <span>{item.username}</span>
-                    </div>
-                    <p>{item.desc}</p>
-                    <div className="star">
-                        <img src={star} alt="" id="starimg" />
-                        <span>{item.star}</span>
-                    </div>
-                </div>
-                <hr />
-
-
-                <div className="details">
-                    <img src={like} alt="" id="#likeimg"/>
-                    <div className="price">
-                        <span>{item.price}$</span>
-                        <span>STARTING AT</span>
-                        <h2>{item.price}</h2>
-                        <h1>{item.desc}</h1>
-                    </div>     
-                 </div>
-                
-            </div>
-
-        </Link>
-    );
-}
+const GigCard = ({ item, index }) => {
+  console.log(index);
+  return (
+    <Link to="/gig" className="link">
+      <div className="gigCard">
+        <img src={item.pp} alt="Projet" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src={star} alt="" id="starimg" />
+            <span>{item.star}</span>
+          </div>
+          <div className="like"></div>
+        </div>
+        <hr />
+        <div className="details">
+          <div className="item-info">
+            <span>{item.desc}</span>
+            <span className="price">STARTING AT {item.price}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default GigCard;
